99d87c3e4e3e5c5ca51e27dc8f3395a34cf725fb
Carlos Ferreira <carlos@especializati.com.br>
2023-03-29 09:56:04 -0300

    feat: primitive types: numbers math

---
+++ 01-primitive-types/07-numbers-math.py
@@ -0,0 +1,26 @@
+import math
+
+x = 3.14
+y = -2
+print(round(x))
+print(abs(y))
+
+
+# MATH
+print(math.ceil(x))
+
+# Calcular o valor absoluto de um número:
+a = -42
+b = 3.14
+print(math.fabs(a))
+print(math.fabs(b))
+
+# Calcular a raiz quadrada de um número:
+a = 49
+print(math.sqrt(a))
+
+# Calcular o seno, cosseno e tangente de um ângulo em radianos:
+angle = math.pi / 4
+print(math.sin(angle))
+print(math.cos(angle))
+print(math.tan(angle))
