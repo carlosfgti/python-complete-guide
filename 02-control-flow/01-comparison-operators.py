1210c558272546370aecc5a5d1766b738da1fca8
Carlos Ferreira <carlos@especializati.com.br>
2023-03-29 10:06:20 -0300

    feat:  control flow: comparison operators

---
+++ 02-control-flow/01-comparison-operators.py
@@ -0,0 +1,13 @@
+print(20 > 3)
+print(20 >= 3)
+print(20 < 30)
+print(20 <= 40)
+print(20 == 20)
+print(20 != 30)
+
+print('--')
+
+print("amazon" > "umnomedourado")
+print("amazon" > "AMAZON")
+print(ord("a"))
+print(ord("A"))
