b503ed3581ddfbbbc6758b9f4a6b952c2d875251
Carlos Ferreira <carlos@especializati.com.br>
2023-03-29 09:53:29 -0300

    feat: primitive types: string methods

---
+++ 01-primitive-types/05-string-methods.py
@@ -0,0 +1,15 @@
+name = " carlos ferreira "
+print(len(name))
+print(name.upper()) # return a new string
+print(name)
+print(name.lower())
+print(name.title())
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+print(name.find("fer")) # python is case sentive
+print(name.find("Fer")) # -1
+print(name.replace(" ", ""))
+print(len(name.replace(" ", "")))
+print("fer" in name)
+print("fer" not in name)
