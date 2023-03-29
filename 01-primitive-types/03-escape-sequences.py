6daab4eaa7547cd7b405b62f308fec144b124eaf
Carlos Ferreira <carlos@especializati.com.br>
2023-03-29 09:51:11 -0300

    feat: primitive types: escape sequences

---
+++ 01-primitive-types/03-escape-sequences.py
@@ -0,0 +1,17 @@
+string = "Hello\nWorld"
+print(string)
+
+string = "Line 1\nLine 2"
+print(string)
+
+string = "Column 1\tColumn 2"
+print(string)
+
+string = "C:\\Windows\\System32"
+print(string)
+
+string = 'This is a single-quoted string with an apostrophe (\') inside.'
+print(string)
+
+string = "This is a double-quoted string with a quotation mark (\") inside."
+print(string)
