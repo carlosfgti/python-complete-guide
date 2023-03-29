bae848cdc0a5e7fb6978bc3a202130cbc0e87f43
Carlos Ferreira <carlos@especializati.com.br>
2023-03-29 09:57:37 -0300

    feat: primitive types: type conversion

---
+++ 01-primitive-types/08-type-conversion.py
@@ -0,0 +1,12 @@
+x = input("Informe um valor para x: ")
+y = input("Informe um valor para y: ")
+print(type(x), type(y))
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+result = int(x) + int(y)
+
+print(f"O resultado da soma de x + y é: {result}")
