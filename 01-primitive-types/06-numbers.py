36c93c92bf8b631809f2915fa5f4d955942f7d98
Carlos Ferreira <carlos@especializati.com.br>
2023-03-29 09:54:37 -0300

    feat: primitive types: numbers

---
+++ 01-primitive-types/06-numbers.py
@@ -0,0 +1,26 @@
+# integer
+a = 42
+b = -7
+c = 0
+
+# (float)
+a = 3.14
+b = -0.9
+c = 0.0
+
+# (complex):
+a = 3 + 4j
+b = -5j
+c = 1 + 0j
+
+print(20 + 3)
+print(20 - 3)
+print(20 * 3)
+print(20 / 3)
+print(20 // 3)
+print(20 % 2)
+print(20 ** 2)
+
+x = 10
+x = x + 10
+x += 10
