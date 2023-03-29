c019606ef86e6c271f7752b9259aebd183bd81c0
Carlos Ferreira <carlos@especializati.com.br>
2023-03-29 09:49:53 -0300

    feat: primitive types: strings

---
+++ 01-primitive-types/02-strings.py
@@ -0,0 +1,12 @@
+text = """Learn Python
+Programming"""
+print(text)
+name = "Carlos Ferreira"
+print(name)
+
+print(len(name))
+print(name[0])
+print(name[0:6])
+print(name[:6])
+print(name[7:])
+print(name[-8])
