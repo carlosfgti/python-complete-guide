4c571fd65365ce577539e62cceccc632e869b908
Carlos Ferreira <carlos@especializati.com.br>
2023-03-30 09:47:51 -0300

    feat: functions: arguments

---
+++ 03-functions/02-arguments.py
@@ -0,0 +1,9 @@
+def sum(number_one, number_two):
+    print(number_one + number_two)
+
+sum(2, 3)
+
+def fullname(name, last_name):
+    print(f"{name} {last_name}")
+
+fullname("Carlos", "Ferreira")
