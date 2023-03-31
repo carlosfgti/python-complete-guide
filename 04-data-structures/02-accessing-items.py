357ed2cfb7353e8f9eefe1482b321fece9d44ce7
Carlos Ferreira <carlos@especializati.com.br>
2023-03-31 08:52:01 -0300

    feat: data structures: accessing items

---
+++ 04-data-structures/02-accessing-items.py
@@ -0,0 +1,12 @@
+letters = ["a", "b", "c"]
+print(letters[0])
+letters[0] = "A"
+print(letters[0])
+print(letters[-1])
+print(letters[0:2])
+print(letters[1:])
+print(letters[::2])
+
+print("----------")
+numbers = list(range(10))
+print(numbers[::-1]) # change order
