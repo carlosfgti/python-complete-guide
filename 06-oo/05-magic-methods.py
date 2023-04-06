aae66337623c306031a7b4c3cdb3ab6914b642c2
Carlos Ferreira <carlos@especializati.com.br>
2023-04-06 09:22:28 -0300

    feat: oo - magic methods

---
+++ 06-oo/05-magic-methods.py
@@ -0,0 +1,24 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "-({}, {})-".format(self.x, self.y)
+
+    def __repr__(self):
+        return "Point({}, {})".format(self.x, self.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+p1 = Point(1, 2)
+p2 = Point(3, 4)
+
+print(p1)      
+print(repr(p1))
+print(p1 == p2)
+print(p1 + p2) 
