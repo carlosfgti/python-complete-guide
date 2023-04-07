7462b18d07d14738fe1de397fe7b06b4f284d957
Carlos Ferreira <carlos@especializati.com.br>
2023-04-07 10:10:33 -0300

    feat: oo - interfaces

---
+++ 06-oo/08-interfaces.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from typing import Protocol
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+    def perimeter(self):
+        return 4 * self.side
+
+class Shape(Protocol):
+    def area(self) -> float:
+        pass
+
+    def perimeter(self) -> float:
+        pass
+
+class Circle:
+    def __init__(self, radius: float):
+        self.radius = radius
+
+    def area(self) -> float:
+        return 3.14 * self.radius ** 2
+
+    def perimeter(self) -> float:
+        return 2 * 3.14 * self.radius
+
+class Square:
+    def __init__(self, side: float):
+        self.side = side
+
+    def area(self) -> float:
+        return self.side ** 2
+
+    def perimeter(self) -> float:
+        return 4 * self.side
