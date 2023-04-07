856e1c6a3b4dee791fac535021696d1b292e8f0a
Carlos Ferreira <carlos@especializati.com.br>
2023-04-07 10:25:47 -0300

    feat: modules - packages

---
+++ 07-modules/03-packages.py
@@ -0,0 +1,5 @@
+import cart.tax as cart
+from cart.tax import calc_tax
+
+print(cart.apply_discount(100))
+print(calc_tax(100))
---
+++ 07-modules/cart/tax.py
@@ -0,0 +1,5 @@
+def calc_tax(value):
+    return value * 0.2
+
+def apply_discount(value):
+    return value - (value * 0.2)
