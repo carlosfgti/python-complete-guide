bf6c00006952c438e46c87255caefbf366c11d38
Carlos Ferreira <carlos@especializati.com.br>
2023-04-13 09:32:44 -0300

    feat: standard library - working with timestamps

---
+++ 08-python-standard-library/08-working-with-timestamps.py
@@ -0,0 +1,12 @@
+import time
+
+# print(time.time())
+def send_email():
+    for i in range(1000):
+        pass
+
+start = time.time()
+send_email()
+end = time.time()
+duration = end = start
+print(f"duration: {duration}")
